ebe36837d0ced514c784f62684ffc4dcb448f421
Toni Mahilainen <toni.mahilainen@student.careeria.fi>
2020-07-15 15:18:58 +0300

    Custom date picker valmis. Deploy ongelma

---
+++ IsItMyTurn.Android/CustomRenderers/CustomDatePickerAndroid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using IsItMyTurn.CustomRenderers;
+using IsItMyTurn.Droid.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomDatePicker), typeof(CustomDatePickerAndroid))]
+namespace IsItMyTurn.Droid.CustomRenderers
+{
+    public class CustomDatePickerAndroid : DatePickerRenderer
+    {
+        public CustomDatePickerAndroid(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                GradientDrawable gradientDrawable = new GradientDrawable();
+                gradientDrawable.SetCornerRadius(10f);
+                gradientDrawable.SetColor(Android.Graphics.Color.Black);
+                gradientDrawable.SetAlpha(153);
+                Control.SetPadding(20, 10, 20, 10);
+                Control.Gravity = GravityFlags.Center;
+                Control.SetBackground(gradientDrawable);
+            }
+        }
+    }
+}
---
+++ IsItMyTurn.iOS/CustomRenderers/CustomDatePickerIos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using IsItMyTurn.CustomRenderers;
+using IsItMyTurn.iOS.CustomRenderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomDatePicker), typeof(CustomDatePickerIos))]
+namespace IsItMyTurn.iOS.CustomRenderers
+{
+    public class CustomDatePickerIos : DatePickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                Control.Layer.CornerRadius = 15;
+                Control.Layer.BackgroundColor = Color.Black.ToCGColor();
+                Control.Layer.Opacity = 0.6f;
+                Control.TextColor = Color.White.ToUIColor();
+                Control.TextAlignment = UITextAlignment.Center;
+            }
+        }
+    }
+}
